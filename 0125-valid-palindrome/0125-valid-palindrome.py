aafd45e4e0a5d865c10e10b78d5128093ae54fe8
havegrit <79460763+havegrit@users.noreply.github.com>
2024-03-26 23:58:54 +0900

    Time: 764 ms (5.33%), Space: 17.2 MB (47.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -9,12 +9,6 @@ class Solution:
             else:
                 s = s.replace(char, '')
             
-        reversedString = ""
+        reversedString = s[::-1]
         
-        for i in range(len(s)-1, -1, -1):
-            reversedString += s[i]
-            
-        if s != reversedString:
-            return False
-        
-        return True
+        return True if reversedString == s else False
