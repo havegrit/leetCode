ab023539ce2dada17d6f5ef2c78202d251e1fdc4
havegrit <79460763+havegrit@users.noreply.github.com>
2024-03-27 18:07:14 +0900

    Time: 149 ms (5.10%), Space: 16.7 MB (44.02%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for char in ransomNote:
+            if not magazine.count(char) >= ransomNote.count(char):
+                return False
+        
+        return True
