6da3fcc2240d256fd7508a2553e1f01456a638a3
havegrit <79460763+havegrit@users.noreply.github.com>
2024-03-28 22:14:37 +0900

    Time: 44 ms (62.26%), Space: 17 MB (63.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
+        s2 = ''
         for char in s:
-            if 'a' <= char <= 'z':
-                pass
-            elif '0' <= char <= '9':
-                pass
-            else:
-                s = s.replace(char, '')
+            if char.isalnum():
+                s2 += char.lower()
             
-        reversedString = s[::-1]
+        reversedString = s2[::-1]
         
-        return True if reversedString == s else False
+        return True if reversedString == s2 else False
